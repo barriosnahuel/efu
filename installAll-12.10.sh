93aeab9964ed6f71facc26b229652e287442720e
Nahuel Barrios <barrios.nahuel@gmail.com>
2013-09-17 01:50:45 -0300

    Add Oracle JDK, FreeMind and Git. Remove lens and scopes.

--- installAll-12.10.sh
+++ installAll-12.10.sh
@@ -10,6 +10,9 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 #Add repository
 sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 
+#Add Oracle JDK repository
+sudo add-apt-repository ppa:webupd8team/java
+
 #Add repositories for the WebApps and Lens.
 sudo add-apt-repository ppa:scopes-packagers/ppa
 
@@ -28,13 +31,8 @@ sudo add-apt-repository ppa:webupd8team/sublime-text-3
 #Add repositories for RabbitVCS integration with Nautilus. Like TortoiseSVN.
 sudo add-apt-repository ppa:rabbitvcs/ppa
 
-#PROBAR CON ESTE COMENTADO, NO ME ACUERDO PARA CUAL ERA NECESARIO
-#sudo add-apt-repository ppa:webapps/preview
-#sudo add-apt-repository ppa:atareao/lenses
-
 #Add repositories for Mixxx DJ software.
 sudo add-apt-repository ppa:mixxx/mixxx
-
 
 #Update software sources. Required after adding a ppa repository.
 sudo apt-get update
@@ -46,47 +44,29 @@ sudo apt-get autoremove
 ##############################  Install favorite software   ##############################
 
 #Install the stable version of Google Chrome
+sudo apt-get purge openjdk*
+sudo apt-get install software-properties-common
 sudo apt-get install google-chrome-stable
 
-#Install OpenJDK plugin to run Java applets from browsers: Firefox, Chromium, Google Chrome, and Epiphany.
-sudo apt-get install icedtea-7-plugin
+#Install Java JDK and set as default.
+sudo apt-get install oracle-jdk7-installer oracle-java7-set-default
+
+#Install FreeMind to make and view mind maps.
+sudo apt-get install freemind
 
 #Install Skype.
-sudo apt-get install skype skype-bin
-
+			sudo apt-get install skype skype-bin
 #Install JDownloader.
 sudo apt-get install jdownloader
 
 #Install GMount ISO.
 sudo apt-get install gmountiso
 
-#####   Install Unity lens and web apps.
-sudo apt-get install unity-lens-news
-sudo apt-get install unity-lens-gdocs
-sudo apt-get install unity-lens-photo
-
 #Add the PirateBay scope for torrents lens. Required when installing torrents lens 'cuz if not then it throws an error. Try without
 sudo apt-get install unity-scope-piratebay python-lxml
-sudo apt-get install unity-lens-torrents
 
 #Install Sublime Text 3 editor (beta).
 sudo apt-get install sublime-text-installer
-
-#Install GitHub len (the following 2 lines)
-sudo apt-get -t quantal-backports install python-unity-singlet
-sudo apt-get install unity-lens-github
-
-#ESTE ESTA FALLANDO POR: unity-webapps-preview : Depends: unity-webapps but it is not going to be installed
-#sudo apt-get install unity-webapps-preview
-
-sudo apt-get install unity-webapps-bbcnews
-sudo apt-get install unity-webapps-facebookapps
-sudo apt-get install unity-webapps-facebookmessenger
-sudo apt-get install unity-webapps-gmail
-sudo apt-get install unity-webapps-googleplus
-sudo apt-get install unity-webapps-linkedin
-sudo apt-get install unity-webapps-twitter
-sudo apt-get install unity-webapps-youtube
 
 #Notificaciones (entrar con Firefox/Chromium a):
 #   No andan: Twitter, Calendar, Gmail, Google+, Linkedin, Facebook, 
@@ -95,14 +75,17 @@ sudo apt-get install unity-webapps-youtube
 
 ##############################  Install development software ##############################
 
+#Install GIT
+sudo apt-get install git-core
+
+#Install Apache Maven for dependencies management.
+sudo apt-get install maven
+
 #RabbitVCS integration with Nautilus. Like TortoiseSVN.
 sudo apt-get install rabbitvcs-nautilus3
 
 #Install Intellij IDEA (Community Edition).
 #sudo apt-get install intellij-idea-ce
-
-#Install Apache Maven for dependencies management.
-sudo apt-get install maven
 
 #####   Install required software to develop Ubuntu apps for mobile devices (this is for create the UI).
 #Install Qt 5
