ff971c823eac156593cdafece214c0ccd15e0427
Nahuel Barrios <barrios.nahuel@gmail.com>
2014-04-28 23:21:10 -0300

    Add Steam client.

--- installAll-14.04.sh
+++ installAll-14.04.sh
@@ -86,8 +86,8 @@ sudo apt-get install oracle-jdk7-installer oracle-java7-set-default
 sudo apt-get install -y grive-tools
 
 #Install JDownloader
-# Dropbox, Freemind (mind maps editor), GMountISO
-sudo apt-get install jdownloader freemind gmountiso
+# Dropbox, Freemind (mind maps editor), GMountISO, Steam client (will update on first usage)
+sudo apt-get install jdownloader freemind gmountiso steam
 
 ##############################      Run final commands      ##############################
 
