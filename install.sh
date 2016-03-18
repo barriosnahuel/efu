6a0d34c158e01e3572f3480c7b9f8eb844d871b9
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-03-17 23:21:39 -0300

    Also install Groovy via sdkman.

--- install.sh
+++ install.sh
@@ -44,9 +44,9 @@ log "Loading common software installation"
 log "Common software installed ok"
 
 
-log "Loading $PLATFORM custom installation file..."
-if [ "$isOsx" ]; then
-    . ./OS\ X/installAll-10.9.sh
-else
-    . ./Ubuntu/installAll-15-04.sh
-fi
+#log "Loading $PLATFORM custom installation file..."
+#if [ "$isOsx" ]; then
+#    . ./OS\ X/installAll-10.9.sh
+#else
+#    . ./Ubuntu/installAll-15-04.sh
+#fi
