8d22d90d20f3b0afce97667c0fe0616e67c250da
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-03-19 00:03:17 -0300

    Fix shellcheck issues.

--- modules/android.sh
+++ modules/android.sh
@@ -13,18 +13,18 @@ if isUbuntu "$PLATFORM"; then
     addToShell "export IBUS_ENABLE_SYNC_MODE=1"
 fi
 
-log "Configuring Android SDK environment variables..." &&
+log "Configuring Android SDK environment variables..."
 
 # shellcheck disable=SC2016
-addToShell 'export ANDROID_HOME=$HOME/Coding/xDKs/android-sdk' &&
+addToShell 'export ANDROID_HOME=$HOME/Coding/xDKs/android-sdk'
 
 # shellcheck disable=SC2016
-addToShell 'export PATH=$ANDROID_HOME/tools:$PATH' &&
+addToShell 'export PATH=$ANDROID_HOME/tools:$PATH'
 
 # shellcheck disable=SC2016
-addToShell 'export PATH=$ANDROID_HOME/platform-tools:$PATH' &&
+addToShell 'export PATH=$ANDROID_HOME/platform-tools:$PATH'
 
 # This is to be able to use the hierarchy viewer
-addToShell 'export ANDROID_HVPROTO=ddm' &&
+addToShell "export ANDROID_HVPROTO=ddm"
 
 log "Successfully added Android environment variables."
