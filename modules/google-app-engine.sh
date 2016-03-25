00b1873bf7200389ced60417ef41de334954c2ea
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-03-25 16:37:39 -0300

    Minor improvement in google-app-engine.sh

--- modules/google-app-engine.sh
+++ modules/google-app-engine.sh
@@ -10,3 +10,4 @@ cd ~/Downloads/
 addToShell 'export PATH=$PATH:$HOME/Coding/xDKs/appengine-java-sdk-1.9.34/'
 
 postInstallationLog "Google App Engine SDK"
+logInfo "Remember that Google App Engine requires Java7 to compile. If you're using Java8 then be sure to set the javac compiler flags to generate 1.7 bytecode: '-source 1.7 -target 1.7'"
