66191e8223171767ba1b4e2235d438bb23bb8c5b
Nahuel Barrios <barrios.nahuel@gmail.com>
2016-05-11 00:41:23 -0300

    Fix Java installation. fixes #52

--- modules/java.sh
+++ modules/java.sh
@@ -2,7 +2,9 @@
 # Created by Nahuel Barrios on 24/3/16.
 
 
-if ! command -v java >/dev/null; then
+if grep "JAVA_HOME" ~/.zshrc; then
+   logAlreadyInstalled "Java"
+else
     logInfo "Download latest Java7 from: http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html"
     logInfo "Download latest Java8 from: http://www.oracle.com/technetwork/java/javase/downloads/java-archive-javase8-2177648.html"
     logInfo "Remember to update JAVA_HOME environment variable after installing Java"
@@ -12,8 +14,6 @@ if ! command -v java >/dev/null; then
 
     # shellcheck disable=SC2016
     addToShell '#export PATH=$JAVA_HOME/bin:$PATH'
-else
-    logAlreadyInstalled "Java"
 fi
 
 logProgramVersion "Java" "$(java version)"
