db2b567d7c31bd1c4204722468b83f054cd5f844
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-04-14 17:05:20 -0300

    Fix shellcheck warnings

--- modules/maven.sh
+++ modules/maven.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Created by Nahuel Barrios on 25/3/16.
+# shellcheck disable=SC2016
 
 cd "${CURRENT_DIR}" || (echo "Failed cding into EFU's execution directory, exiting..." && exit)
 
@@ -11,10 +12,7 @@ if ! command -v mvn >/dev/null; then
         cd ~/Coding/xDKs/ || (echo "Failed cding into xDKs directory, exiting..." && exit)
         downloadAndUncompress "Apache Maven" "http://mirrors.nxnethosting.com/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip" &&
 
-        # shellcheck disable=SC2016
         addToShell 'export M2_HOME=$HOME/Coding/xDKs/apache-maven-3.3.9'
-
-        # shellcheck disable=SC2016
         addToShell 'export PATH=$M2_HOME/bin:$PATH'
 
     else
--- modules/sdkman/sdkman.sh
+++ modules/sdkman/sdkman.sh
@@ -7,8 +7,8 @@ if ! command -v sdk >/dev/null; then
     preInstallationLog "sdkman"
     export SDKMAN_DIR="$HOME/Coding/xDKs/sdkman" && curl -s get.sdkman.io | bash &&
 
-    # shellcheck source=/dev/null
     source "$HOME/Coding/xDKs/sdkman/bin/sdkman-init.sh"
+
     postInstallationLog "sdkman"
 else
     logAlreadyInstalled "sdkman"
@@ -20,5 +20,4 @@ cp modules/sdkman/gradle.properties ~/.gradle/ &&
 log "Gradle properties created successfully. Check ~/.gradle/gradle.properties to see current configuration"
 
 # This is required in order to apply previous installs for current terminal session.
-# shellcheck source=/dev/null
 source "$HOME/Coding/xDKs/sdkman/bin/sdkman-init.sh"
