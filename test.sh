ad1e55b621ac2a3157eee89daa28ad28725c8e6c
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-06-12 00:56:16 -0300

    Add base test.sh script for manual testing purposes

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+###
+# Setup method for testing purposes.
+# Compiles functions in order to be able to test prod files.
+##
+setup () {
+    . ./functions.sh
+}
+
+setup
+
+###
+# Code your test after this comment...
+##
+
