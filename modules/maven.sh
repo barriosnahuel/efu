e84f58bf4be059e91d308fee71b473916c0861ad
Nahuel Barrios <barrios.nahuel@gmail.com>
2019-06-12 23:20:59 -0300

    Better organization in .zshrc and .bashrc for Apache Maven

--- modules/maven.sh
+++ modules/maven.sh
@@ -11,8 +11,13 @@ if ! command -v mvn >/dev/null; then
         enterDirOrExit ~/Coding/xDKs/
         downloadAndUncompress "Apache Maven" "http://mirrors.nxnethosting.com/apache/maven/maven-3/3.3.9/binaries/apache-maven-3.3.9-bin.zip" &&
 
+
+        addToShell "###"
+        addToShell "# Apache Maven env vars"
+        addToShell "##"
         addToShell "export M2_HOME=\$HOME/Coding/xDKs/apache-maven-3.3.9"
         addToShell "export PATH=\$M2_HOME/bin:\$PATH"
+        addToShell ""
 
     else
         sdk install maven
