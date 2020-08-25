c0ba95c6f580a093e3fddfba067d56fa94743fbf
Nahuel Barrios <barrios.nahuel@gmail.com>
2020-08-24 22:19:45 -0300

    Add Java env vars to .zshrc

--- modules/java.sh
+++ modules/java.sh
@@ -5,6 +5,12 @@ enterDirOrExit "${CURRENT_DIR}"
 
 if ! command -v java >/dev/null; then
   logSummary "Remember to download the Java Development Toolkit (JDK) from: https://www.oracle.com/ar/java/technologies/javase-downloads.html"
+
+  addToShell "###"
+  addToShell "# Java SDK env vars"
+  addToShell "##"
+  addToShell "export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home"
+
 else
   logAlreadyInstalled "Java"
   logProgramVersion "Java" "$(java -version)"
