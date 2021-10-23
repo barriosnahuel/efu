1a8d16b60496a6ff90137c9d9fa1ae5f35e45cc4
Nahuel Barrios <barrios.nahuel@gmail.com>
2021-10-23 14:14:02 -0300

    Update JDK 8 env var to latest version

--- modules/java.sh
+++ modules/java.sh
@@ -9,7 +9,7 @@ if ! command -v java >/dev/null; then
   addToShell "###"
   addToShell "# Java SDK env vars"
   addToShell "##"
-  addToShell "export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_261.jdk/Contents/Home"
+  addToShell "export JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk1.8.0_311.jdk/Contents/Home"
 
 else
   logAlreadyInstalled "Java"
